b912f9f1bca3907204d7c7b853189649e8a396e1
Illya Moskvin <imoskvin@artic.edu>
2017-04-25 10:16:01 -0500

    Guard against missing fields

--- collections.rb
+++ collections.rb
@@ -2,6 +2,11 @@ require 'grape'
 require 'rsolr'
 require './conf.rb'
 
+# Sample calls:
+#   http://localhost:9292/v1/artworks/111628
+#   http://localhost:9292/v1/artworks/111629
+#   http://localhost:9292/v1/artworks/60622 (missing history fields)
+
 module Collections
   class API < Grape::API
     version 'v1'
@@ -29,51 +34,76 @@ module Collections
 
           data = input["response"]["docs"][0]
 
+          # Uncomment this to see all available fields
+          # We are aiming to use the LPM fields only, for forwards compatibility
+          # Everything below the `id` field is drawn from CITI's Web Solr instance
+          # return data
+
+          def data.get( key, unwrap = true, int = false)
+
+            if !self.key?(key)
+              return nil
+            end
+
+            out = self[key]
+
+            if unwrap
+              out = out[0]
+            end
+
+            if int
+              out = out.to_i
+            end
+
+            return out
+
+          end
+
           output = {
             "ids": {
-              "citi": data["citiUid"][0].to_i,
-              "main": data["mainRefNumber"][0],
+              "citi": data.get("citiUid", true, true),
+              "main": data.get("mainRefNumber"),
               "lake": {
-                "hid": data["uid"][0],
-                "guid": data["id"],
-                "batch": data["batchUid"][0],
-                "uri": data["uri"],
+                "hid": data.get("uid"),
+                "guid": data.get("id", false),
+                "batch": data.get("batchUid"),
+                "uri": data.get("uri", false),
               },
             },
             "title": {
-              "raw": data["title"][0],
-              "display": data["prefLabel"][0],
+              "raw": data.get("title"),
+              "display": data.get("prefLabel"),
             },
             "dates": {
-              "start": data["earliestYear"][0].to_i,
-              "end": data["latestYear"][0].to_i,
-              "display": data["dateDisplay"][0],
-            },
-            "department": {
-              "id": data["department_uid"][0],
-              "display": data["department"][0],
+              "start": data.get("earliestYear", true, true),
+              "end": data.get("latestYear", true, true),
+              "display": data.get("dateDisplay"),
             },
             "creator": {
-              "id": data["artist_uid"][0],
-              "raw": data["artist"][0],
-              "display": data["creatorDisplay"][0],
+              "id": data.get("artist_uid"),
+              "raw": data.get("artist"),
+              "display": data.get("creatorDisplay"),
             },
-            "dimensions": data["dimensionsDisplay"][0],
+            "department": {
+              "id": data.get("department_uid"),
+              "display": data.get("department"),
+            },
+            "dimensions": data.get("dimensionsDisplay"),
             "medium": {
-              "raw": data["medium"][0],
-              "display": data["mediumDisplay"][0],
+              "raw": data.get("medium"),
+              "display": data.get("mediumDisplay"),
             },
-            "inscriptions": data["inscriptions"],
-            "credit_line": data["creditLine"][0],
+            "inscriptions": data.get("title", false),
+            "credit_line": data.get("creditLine"),
             "history": {
-              "publications": data["publicationHistory"][0],
-              "exhibitions": data["exhibitionHistory"][0],
-              "provenance": data["provenanceText"][0],
+              "publications": data.get("publicationHistory"),
+              "exhibitions": data.get("exhibitionHistory"),
+              "provenance": data.get("provenanceText"),
             },
-            "created_at": data["created"][0],
-            "created_by": data["createdBy"][0],
-            "modified_at": data["lastModified"][0],
-            "modified_by": data["lastModifiedBy"][0],
+            "created_at": data.get("created"),
+            "created_by": data.get("createdBy"),
+            "modified_at": data.get("lastModified"),
+            "modified_by": data.get("lastModifiedBy"),
           }
 
         end
