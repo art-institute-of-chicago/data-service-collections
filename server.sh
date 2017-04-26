f8cd6192ffb9b927202c41e609e029ce7caa6fbe
Illya Moskvin <imoskvin@artic.edu>
2017-04-26 10:34:38 -0500

    Kill shotgun when Dredd finishes test

--- server.sh
+++ server.sh
@@ -1,4 +1,20 @@
 # Use this to run server silently
 # Useful for cleaning up Dredd's output
 # https://github.com/apiaryio/dredd/issues/593
-shotgun > /dev/null 2>&1
+
+# Run shotgun, redirect all output to null (stdout and stderr)
+# Launch it as forked, backgrounded child process
+shotgun > /dev/null 2>&1 &
+
+# Save its process id
+PID=$!
+
+# Kill shotgun on exit
+function cleanup {
+   kill $PID
+}
+
+trap cleanup EXIT
+
+# Keep this script running until Dredd sends SIGKILL
+cat
