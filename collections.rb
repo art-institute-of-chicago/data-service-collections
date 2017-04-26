e0d8d5ab515b39702cde4442fd2fa8e39a62b2e3
nikhiltri <me@nikhiltrivedi.com>
2017-04-26 17:11:00 -0500

    Add endpoint for querying multiple artworks

--- collections.rb
+++ collections.rb
@@ -34,6 +34,55 @@ module Collections
     end
 
     resource :artworks do
+      desc 'Return a few artworks.'
+      params do
+        requires :ids, type: String, desc: 'Artwork IDs'
+      end
+      route_param :ids do
+        get do
+          
+          if '[0-9,]+'.match(params[:ids].to_s)
+            error!({
+              error: 'Malformed Artwork IDs',
+              detail: 'IDs must be a single number ID, or a list of IDs separated by commas.'
+            }, 400)
+          end
+
+          solr_q = ''
+          params[:ids].split(',').each do |id|
+            if solr_q != ''
+              solr_q.concat(' OR ')
+            end
+            solr_q.concat(id)
+          end
+          solr_q = 'citiUid:('.concat(solr_q).concat(')')
+        
+          # https://github.com/rsolr/rsolr
+          input = @solr.get 'select', params: {
+            fq: 'hasModel:Work AND '.concat(solr_q),
+            q: '*:*',
+            rows: 1000,
+            wt: :ruby
+          }
+        
+          # Abort if no results
+          if input["response"]["numFound"] < 1
+            error!({
+              error: 'Artwork not found',
+              detail: 'Artwork does not exist in LPM Solr. Ensure you are passing the CITI ID.'
+            }, 404)
+          end
+
+          datas = input["response"]["docs"]
+
+          # Uncomment this to see all available fields
+          # return datas
+
+          {
+            "data": API.transform(datas)
+          }
+        end
+      end
 
       desc 'Return an artwork.'
       params do
@@ -73,10 +122,21 @@ module Collections
       end
     end
 
-    def self.transform(data)
+    def self.transform(datas)
 
-    	# This allows data to use the get method
-    	data.extend LakeUnwrapper
+      if datas.kind_of?(Array)
+        ret = []
+        datas.each do |data|
+          ret.push(self._transform(data))
+        end
+        return ret
+      end
+      self._transform(datas)
+    end
+
+    def self._transform(data)
+      # This allows data to use the get method
+      data.extend LakeUnwrapper
 
       # We are aiming to use the LPM fields only, for forwards compatibility
       # Everything below the `id` field is drawn from CITI's Web Solr instance
@@ -129,7 +189,6 @@ module Collections
         "modified_at": data.get("lastModified"),
         "modified_by": data.get("lastModifiedBy"),
       }
-
     end
   end
 end
