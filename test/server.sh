764ce94dcda544ae0d7c56726212cbee5e0563f1
Illya Moskvin <imoskvin@artic.edu>
2017-04-27 13:49:08 -0500

    Move testing things to subfolder

--- test/server.sh
+++ test/server.sh
@@ -2,6 +2,12 @@
 # Useful for cleaning up Dredd's output
 # https://github.com/apiaryio/dredd/issues/593
 
+# Ensure we are in the directory where this script resides
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Break out of the test folder
+cd ../
+
 # Run shotgun, redirect all output to null (stdout and stderr)
 # Launch it as forked, backgrounded child process
 shotgun > /dev/null 2>&1 &
