87fb28b347853ea2a28870537bf301a89907ad1c
Illya Moskvin <imoskvin@artic.edu>
2017-04-28 14:57:20 -0500

    Require helpers via glob

--- config/app.rb
+++ config/app.rb
@@ -2,9 +2,13 @@ require 'grape'
 require 'rsolr'
 
 require_relative 'conf.rb'
-require_relative '../helpers/error_formatter.rb'
-require_relative '../helpers/lake_unwrapper.rb'
+
+Dir.glob(File.expand_path('../helpers/*.rb', __dir__)).each do |file|
+  require_relative file
+end
+
 require_relative '../models/base_model.rb'
+
 Dir.glob(File.expand_path('../models/*.rb', __dir__)).each do |file|
   require_relative file
 end
