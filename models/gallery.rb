e18a9a72d540debc1d7c0fe0255e466e1f042516
Illya Moskvin <imoskvin@artic.edu>
2017-06-12 17:15:10 -0500

    Explicitly limit Place query to galleries

--- models/gallery.rb
+++ models/gallery.rb
@@ -3,6 +3,7 @@ class Gallery < BaseModel
   def initialize
     super
     self.fq = 'hasModel:Place'
+    self.fq << ' AND locationType_uri:"AIC Gallery"'
   end
 
   # TODO: Abstract boolean into lake_unwrapper.rb (?)
