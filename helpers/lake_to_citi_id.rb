edaa69d506adb418f3bf8cd6004c9ad8cb259064
Illya Moskvin <imoskvin@artic.edu>
2018-01-05 16:41:01 -0600

    Make Lake2Citi accept array values

--- helpers/lake_to_citi_id.rb
+++ helpers/lake_to_citi_id.rb
@@ -1,4 +1,23 @@
+# Changes namespaced LAKE UIDs to original CITI ones
+# Accepts arrays or single values
+
 def Lake2Citi(value)
+
+  if value.kind_of?(Array)
+    return value.map { |n| _Lake2Citi(n) }
+  end
+
+  return _Lake2Citi(value)
+
+end
+
+
+# Helper function, don't use directly
+def _Lake2Citi(value)
+
+  if value == nil
+    return nil
+  end
 
   begin
     Integer( /[0-9]+/.match(value)[0] )
