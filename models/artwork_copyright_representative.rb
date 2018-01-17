7ba9a84d84cca98460697dc903a72eea34beec75
Illya Moskvin <imoskvin@artic.edu>
2018-01-17 16:31:22 -0600

    Ignore preferred for copyright representatives

--- models/artwork_copyright_representative.rb
+++ models/artwork_copyright_representative.rb
@@ -10,12 +10,17 @@ class ArtworkCopyrightRepresentative < BaseModel
     # Works have the following fields:
     # objectCopyrightRepresentatives, objectCopyrightRepresentatives_uris, objectCopyrightRepresentatives_uids
 
-    ret[:is_preferred] =  data.get(:isPreferred, false) === "true" # isPreferred": "true"
+    # Interestingly, ObjectCopyrightRepresentatives are never preferred
+    # ret[:is_preferred] =  data.get(:isPreferred, false) === "true" # isPreferred": "true"
 
     # TODO: Pull in an identifier that links to the actual Agent
     # agent_uid, agent_uri, agentName
 
+    ret[:agent_id] = Lake2Citi( data.get(:agent_uid) )
+
     # agentRoleName can be ignored
+
+    # Waiting on Redmine #2424 to resolve this
 
     ret
 
