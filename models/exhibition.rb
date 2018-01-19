20e313d80531178c002f22cd504b5794cfa74564
nikhiltri <ntrivedi@artic.edu>
2018-01-18 21:48:57 -0600

    Clean up exhibition output

--- models/exhibition.rb
+++ models/exhibition.rb
@@ -20,14 +20,14 @@ class Exhibition < BaseModel
 
     ret[:image_guid] = Uri2Guid( data.get(:hasPreferredRepresentation_uri) )
 
-    ret[:aic_start_date] = Time.parse(data.get(:aicStartDate, false)).utc.iso8601
-    ret[:aic_end_date] = Time.parse(data.get(:aicEndDate, false)).utc.iso8601
+    ret[:aic_start_date] = Time.parse(data.get(:aicStartDate, false)).utc.iso8601 rescue nil
+    ret[:aic_end_date] = Time.parse(data.get(:aicEndDate, false)).utc.iso8601 rescue nil
 
-    ret[:start_date] = Time.parse(data.get(:startDate, false)).utc.iso8601
-    ret[:end_date] = Time.parse(data.get(:endDate, false)).utc.iso8601
+    ret[:start_date] = Time.parse(data.get(:startDate, false)).utc.iso8601 rescue nil
+    ret[:end_date] = Time.parse(data.get(:endDate, false)).utc.iso8601 rescue nil
 
     # exhibitionPlace, exhibitionPlace_uris, exhibitionPlace_uids
-    ret[:exhibition_place_ids] = str2int( data.get(:exhibitionPlace_uid, false) )
+    ret[:exhibition_agent_ids] = str2int( data.get(:exhibitionPlace_uid, false) )
 
     # exhibitionObject, exhibitionObject_uris, exhibitionObject_uids
     # TODO: There's currently no data here. Revisit this when we can see what we're getting.
