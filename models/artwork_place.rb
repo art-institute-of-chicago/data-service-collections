8bc816f435b1e15612ea95ab1ef53a1f0ff1e8c8
Illya Moskvin <imoskvin@artic.edu>
2018-02-08 17:58:56 -0600

    Add place_id to ArtworkPlace [#2425]

--- models/artwork_place.rb
+++ models/artwork_place.rb
@@ -12,16 +12,11 @@ class ArtworkPlace < BaseModel
 
     ret[:is_preferred] = data.get(:isPreferred, false) === "true" # isPreferred": "true"
 
-    # TODO: There's no prefLabel. Use locationName instead?
-    # If we could import places, we could use this model as a simple pivot,
-    # but LPM Solr doesn't provide such fields currently.
+    # location_uid, location_uri, locationName
+    ret[:place_id] = Lake2Citi( data.get(:location_uid) )
 
-    # TODO: Import place qualifiers. ObjectPlace has the following fields in LPM Solr:
     # qualifier_uid, qualifier_uri, qualifierText
-
     ret[:place_qualifier_id] = Lake2Citi( data.get(:qualifier_uid) )
-
-    # Waiting on Redmine #2425 to resolve this
 
     ret
 
