4f95196b38eae3056aed3d5f0289540384b48ece
nikhiltri <ntrivedi@artic.edu>
2018-03-29 15:40:22 -0500

    Only get terms of the types we're interested in using on the website

--- models/term.rb
+++ models/term.rb
@@ -2,8 +2,7 @@ class Term < BaseModel
 
   def initialize
     super
-    self.fq = 'hasModel:Term'
-    # TODO: Verify that we aren't importing terms of unwanted types
+    self.fq = 'hasModel:Term AND (termType_uid:TT-1 OR termType_uid:TT-2 OR termType_uid:TT-3 OR termType_uid:TT-4 OR termType_uid:TT-5)'
   end
 
   def transform( data, ret )
