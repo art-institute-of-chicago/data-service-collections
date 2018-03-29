37de71c535c94879a1eac5ac31e023c35870b876
nikhiltri <ntrivedi@artic.edu>
2018-03-29 12:29:50 -0500

    Limit term types to just those we're interested in using in the website

--- models/term_type.rb
+++ models/term_type.rb
@@ -2,7 +2,12 @@ class TermType < BaseModel
 
   def initialize
     super
-    self.fq = 'hasModel:TermType'
+    # TT-1 = Classification
+    # TT-2 = Media/Material
+    # TT-3 = Process/Techniques
+    # TT-4 = Style/Period
+    # TT-5 = Subject
+    self.fq = 'hasModel:TermType AND (uid:TT-1 OR uid:TT-2 OR uid:TT-3 OR uid:TT-4 OR uid:TT-5)'
   end
 
   def transform( data, ret )
