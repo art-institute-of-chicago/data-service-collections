cca750feace52e2f3706abe14e0925bce7bab400
Illya Moskvin <imoskvin@artic.edu>
2018-04-04 19:15:12 -0500

    Add `alt_image_guids` to Exhibition [#2665]

--- models/exhibition.rb
+++ models/exhibition.rb
@@ -18,8 +18,6 @@ class Exhibition < BaseModel
     ret[:gallery] = data.get(:galleryLocation)
     ret[:gallery_id] = str2int( data.get(:gallery_uid) )
 
-    ret[:image_guid] = Uri2Guid( data.get(:hasPreferredRepresentation_uri) )
-
     ret[:aic_start_date] = Time.parse(data.get(:aicStartDate, false)).utc.iso8601 rescue nil
     ret[:aic_end_date] = Time.parse(data.get(:aicEndDate, false)).utc.iso8601 rescue nil
 
@@ -35,6 +33,29 @@ class Exhibition < BaseModel
     ret[:artwork_ids] = str2int( data.get(:exhibitionObject_uid, false) )
 
     ret[:document_ids] = Uri2Guid( data.get(:hasDocument_uri, false) )
+
+    # Remove prefImage from docs just in case
+    if ret[:document_ids] && ret[:image_guid]
+      ret[:document_ids].delete( ret[:image_guid] )
+    end
+
+    # TODO: Rename this field to image_id
+    # TODO: Rename this field to pref_image_id?
+    ret[:image_guid] = Uri2Guid( data.get(:hasPreferredRepresentation_uri) )
+
+    # TODO: Rename this field to alt_image_id?
+    ret[:alt_image_guids] = Uri2Guid( data.get(:hasRepresentation_uri, false) )
+
+    # Remove the prefImage from the altImages array
+    if ret[:alt_image_guids] && ret[:image_guid]
+      ret[:alt_image_guids].delete( ret[:image_guid] )
+    end
+
+    # Remove documents from altImages
+    # "When [interpretive resources] were migrated, they were added as both representations and documentation"
+    if ret[:alt_image_guids] && ret[:document_ids]
+      ret[:alt_image_guids] = ret[:alt_image_guids] - ret[:document_ids]
+    end
 
     ret
 
