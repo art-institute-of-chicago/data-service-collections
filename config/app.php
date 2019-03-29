8f1cdd2f5c0a6be911f47677f37b98a15ee2116b
Illya Moskvin <imoskvin@artic.edu>
2019-03-29 10:28:09 -0500

    Enable TranslationServiceProvider [WEB-950]

--- config/app.php
+++ config/app.php
@@ -47,7 +47,7 @@ return [
         Illuminate\Queue\QueueServiceProvider::class,
         // Illuminate\Redis\RedisServiceProvider::class,
         // Illuminate\Session\SessionServiceProvider::class,
-        // Illuminate\Translation\TranslationServiceProvider::class,
+        Illuminate\Translation\TranslationServiceProvider::class,
         // Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
