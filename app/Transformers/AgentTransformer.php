e00388d6b2759ed8224d9bb9005e9a04f18053bb
Illya Moskvin <imoskvin@artic.edu>
2020-02-19 11:23:14 -0600

    Fix funky dates in agent place pivots [WEB-150, CITI-3373]

--- app/Transformers/AgentTransformer.php
+++ app/Transformers/AgentTransformer.php
@@ -11,6 +11,55 @@ class AgentTransformer extends BaseTransformer
     {
         return [
             'agent_type_id' => $this->nullZero($datum->agent_type_id),
+            'agent_places' => $this->mapToArray($datum->agent_places, 'getAgentPlace'),
         ];
+    }
+
+    protected function getAgentPlace(Datum $agentPlace)
+    {
+        $agentPlace->place_qualifier_id = $this->nullZero($agentPlace->place_qualifier_id);
+
+        // Null out funky dates, pending upstream fixes
+        $nullPairs = [
+            // ex: 80875 (1657-1818)
+            [
+                'date_earliest' => '0000-00-00T00:00:00.000Z',
+                'date_latest' => '0000-00-00T00:00:00.000Z',
+            ],
+            // ex: 3338 (1925-)
+            [
+                'date_earliest' => '0000-00-00T00:00:00.000Z',
+                'date_latest' => null,
+            ],
+            // ex: 4448 (-1844)
+            [
+                'date_earliest' => null,
+                'date_latest' => '0000-00-00T00:00:00.000Z',
+            ],
+            // ex: 116611 (1/1/4713 BCE)
+            [
+                'date_earliest' => '0000-00-00T00:00:00.000Z',
+                'date_latest' => '4713-01-01T06:00:00.000Z',
+            ],
+            // ex: 106799 (blank)
+            [
+                'date_earliest' => '0000-00-00T00:00:00.000Z',
+                'date_latest' => '19265-01-01T06:00:00.000Z',
+            ],
+        ];
+
+        foreach ($nullPairs as $nullPair) {
+            if ((
+                $agentPlace->date_earliest == $nullPair['date_earliest']
+            ) && (
+                $agentPlace->date_latest == $nullPair['date_latest']
+            )) {
+                $agentPlace->date_earliest = null;
+                $agentPlace->date_latest = null;
+                break;
+            }
+        }
+
+        return $agentPlace;
     }
 }
